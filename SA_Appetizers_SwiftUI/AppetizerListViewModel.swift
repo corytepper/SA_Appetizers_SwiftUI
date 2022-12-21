3e926ae24ed720cbf143b084c43bee3733e3a0f5
Cory Tepper <corytepper@gmail.com>
2022-12-21 16:36:42 -0500

    Connect Network Manager to AppetizerListView, build AppetizerlistViewModel

---
+++ SA_Appetizers_SwiftUI/AppetizerListViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  AppetizerListViewModel.swift
+//  SA_Appetizers_SwiftUI
+//
+//  Created by Cory Tepper on 12/21/22.
+//
+
+import Foundation
+
+final class AppetizerListViewModel: ObservableObject {
+    @Published var appetizers: [Appetizer] = []
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+            
+        }
+    }
+}
