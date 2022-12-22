bb204ae82c97eefa5c81c67a09f059d0a215f9cf
Cory Tepper <corytepper@gmail.com>
2022-12-22 18:11:46 -0500

    build LoadingScreen

---
+++ SA_Appetizers_SwiftUI/Color+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  Color+Ext.swift
+//  SA_Appetizers_SwiftUI
+//
+//  Created by Cory Tepper on 12/22/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
+
