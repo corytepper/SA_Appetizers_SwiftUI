521407c19dac3b0f1aa2b2801ad08874f57657ed
Cory Tepper <corytepper@gmail.com>
2022-12-28 12:45:27 -0500

    Improve keyboard and form UX on AccountView with @FocusState.

--- SA_Appetizers_SwiftUI/Screens/Account/AccountView.swift
+++ SA_Appetizers_SwiftUI/Screens/Account/AccountView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct AccountView: View {
     
     @StateObject var viewModel = AccountViewModel()
+    @FocusState private var focusedTextField: FormTextField?
+    
+    enum FormTextField {
+        case firstName, lastName, email
+    }
     
     var body: some View {
         NavigationView {
@@ -17,8 +22,17 @@ struct AccountView: View {
             Form {
                 Section(header: Text("Personal Info")) {
                     TextField("First Name", text: $viewModel.user.firstName)
+                        .focused($focusedTextField, equals: .firstName)
+                        .onSubmit { focusedTextField = .lastName }
+                        .submitLabel(.next)
                     TextField("Last Name", text: $viewModel.user.lastName)
+                        .focused($focusedTextField, equals: .lastName)
+                        .onSubmit { focusedTextField = .email }
+                        .submitLabel(.next)
                     TextField("Email", text: $viewModel.user.email)
+                        .focused($focusedTextField, equals: .email)
+                        .onSubmit { focusedTextField = nil }
+                        .submitLabel(.continue)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .autocorrectionDisabled(true)
@@ -39,6 +53,11 @@ struct AccountView: View {
             }
             
             .navigationTitle("🤓 Account")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Button("Dismiss") { focusedTextField = nil }
+                }
+            }
         }
         .onAppear {
             viewModel.retrieveUser()
